3c756a165ee9be65e0d36db58b87d03a88f3ebfc
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2024-12-30 18:04:30 -0700

    setup jump

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask
 
+import PetoiRobot
+
 app = Flask(__name__)
 
+PetoiRobot.autoConnect()
+PetoiRobot.sendSkillStr("kup",3)
 
-@app.route("jump")
+
+@app.route("/jump")
 def jump():
-    
+    print("Jump!")
+    PetoiRobot.sendSkillStr("kbf")
+    return "<b>Jumped!</b>"
